3bcf8991f304abaff2e03d2b9d59cb5638cc1623
gvevance <gvevance@gmail.com>
2022-02-15 10:53:24 +0530

    DFS A_mat

---
+++ Graph algos/DFS.py
@@ -0,0 +1,36 @@
+'''
+Depth first search. 2 variants - Adjacency matrix and list to represent graph. O(n^2) in n vertices for Adj matrix ; 
+O(m+n) for Adj list when m = number of edges in the graph, n = number of nodes (m << n^2). '''
+
+from collections import deque
+
+def DFS_matrix(A_mat,vertex) :
+    
+    visited = [0 for _ in range(len(A_mat))]
+    stack = deque()
+    visited[vertex] = 1
+    stack.append(vertex)
+
+    while stack :
+        i = stack[-1]
+        flag = True
+        for j in range(len(A_mat)) :
+            if A_mat[i][j] == 1 and visited[j] == 0 :
+                visited[j] = 1
+                stack.append(j)
+                flag = False 
+        if flag :
+            stack.pop()
+    
+    return [i for i in range(len(A_mat)) if (visited[i] == 1 and i != vertex)]
+
+def DFS_list(A_list,vertex) :
+    pass
+
+if __name__ == "__main__" :
+    
+    vertex = 1
+    A_mat = [[0,1,1,0,0],[1,0,1,0,0],[1,1,0,0,0],[0,0,0,0,1],[0,0,0,1,0]]
+    print(DFS_matrix(A_mat,vertex))
+    # A_list = [[1,2],[0,2],[0,1],[4],[3]]
+    # print(BFS_list(A_list,vertex))
