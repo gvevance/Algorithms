e6a0dda36187fc801de08fcf31cabfad6c772a53
gvevance <gvevance@gmail.com>
2022-02-15 10:56:41 +0530

    DFS A_list

--- Graph algos/DFS.py
+++ Graph algos/DFS.py
@@ -4,6 +4,7 @@ O(m+n) for Adj list when m = number of edges in the graph, n = number of nodes (
 
 from collections import deque
 
+
 def DFS_matrix(A_mat,vertex) :
     
     visited = [0 for _ in range(len(A_mat))]
@@ -24,13 +25,32 @@ def DFS_matrix(A_mat,vertex) :
     
     return [i for i in range(len(A_mat)) if (visited[i] == 1 and i != vertex)]
 
+
 def DFS_list(A_list,vertex) :
-    pass
+
+    visited = [0 for _ in range(len(A_mat))]
+    stack = deque()
+    visited[vertex] = 1
+    stack.append(vertex)
+
+    while stack :
+        i = stack[-1]
+        flag = True
+        for elem in A_list[i] :
+            if visited[elem] == 0 :
+                visited[elem] = 1
+                stack.append(elem)
+                flag = False
+        if flag :
+            stack.pop()
+    
+    return [i for i in range(len(A_mat)) if (visited[i] == 1 and i != vertex)]
+ 
 
 if __name__ == "__main__" :
     
-    vertex = 1
+    vertex = 4
     A_mat = [[0,1,1,0,0],[1,0,1,0,0],[1,1,0,0,0],[0,0,0,0,1],[0,0,0,1,0]]
     print(DFS_matrix(A_mat,vertex))
-    # A_list = [[1,2],[0,2],[0,1],[4],[3]]
-    # print(BFS_list(A_list,vertex))
+    A_list = [[1,2],[0,2],[0,1],[4],[3]]
+    print(DFS_list(A_list,vertex))
