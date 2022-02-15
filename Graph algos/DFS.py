2e0b0f9367e799266d1e40b38ac493462ed1a084
gvevance <gvevance@gmail.com>
2022-02-15 18:53:06 +0530

    DFS logic corrected

--- Graph algos/DFS.py
+++ Graph algos/DFS.py
@@ -1,56 +1,44 @@
 '''
 Depth first search. 2 variants - Adjacency matrix and list to represent graph. O(n^2) in n vertices for Adj matrix ; 
-O(m+n) for Adj list when m = number of edges in the graph, n = number of nodes (m << n^2). '''
+O(m+n) for Adj list when m = number of edges in the graph, n = number of nodes (assuming m << n^2). DFS is inherently 
+recursive in nature. '''
 
 from collections import deque
 
 
-def DFS_matrix(A_mat,vertex) :
+def DFS_list(A,vertex,visited) :
     
-    visited = [0 for _ in range(len(A_mat))]
-    stack = deque()
     visited[vertex] = 1
-    stack.append(vertex)
-
-    while stack :
-        i = stack[-1]
-        flag = True
-        for j in range(len(A_mat)) :
-            if A_mat[i][j] == 1 and visited[j] == 0 :
-                visited[j] = 1
-                stack.append(j)
-                flag = False 
-        if flag :
-            stack.pop()
-    
-    return [i for i in range(len(A_mat)) if (visited[i] == 1 and i != vertex)]
+    for elem in [i for i in A[vertex] if visited[i] == 0] :
+        DFS_list(A,elem,visited)
 
 
-def DFS_list(A_list,vertex) :
+def DFS_matrix(A,vertex,visited) :
 
-    visited = [0 for _ in range(len(A_mat))]
-    stack = deque()
     visited[vertex] = 1
-    stack.append(vertex)
+    for j in range(len(A)) :
+        if A[vertex][j] == 1 and visited[j] == 0 :
+            DFS_matrix(A,j,visited)
 
-    while stack :
-        i = stack[-1]
-        flag = True
-        for elem in A_list[i] :
-            if visited[elem] == 0 :
-                visited[elem] = 1
-                stack.append(elem)
-                flag = False
-        if flag :
-            stack.pop()
+
+def DFS(A,vertex,list) :
+
+    visited = [0 for _ in range(len(A))]        # doesn't matter if A is a matrix or list
+
+    if list == True :
+        DFS_list(A,vertex,visited)
+
+    else :
+        DFS_matrix(A,vertex,visited)
     
-    return [i for i in range(len(A_mat)) if (visited[i] == 1 and i != vertex)]
- 
+    return [i for i in range(len(A)) if (visited[i] == 1 and i != vertex)]
+
 
 if __name__ == "__main__" :
     
-    vertex = 4
-    A_mat = [[0,1,1,0,0],[1,0,1,0,0],[1,1,0,0,0],[0,0,0,0,1],[0,0,0,1,0]]
-    print(DFS_matrix(A_mat,vertex))
-    A_list = [[1,2],[0,2],[0,1],[4],[3]]
-    print(DFS_list(A_list,vertex))
+    vertex = 2
+    A_mat = [[0,1,0],[1,0,0],[0,0,0]]
+    print(DFS(A_mat,vertex,list=False))
+    A_list = [[1],[0],[]]
+    print(DFS(A_list,vertex,list=True))
+    # print(DFS_list_pre_post(A_list,vertex))
